9582ca52648e7216daa1e0f3ac09dbc80ed98441
Eugene Zaretskiy <eugene.zar@gmail.com>
2014-10-23 14:20:15 -0400

    Using environment user/pass

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  db: 'mongodb://localhost/mean-prod',
+  db: 'mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASSWORD + '@linus.mongohq.com:10010/dungeonlords',
   /**
    * Database options that will be passed directly to mongoose.connect
    * Below are some examples.
