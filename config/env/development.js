78d83570ba6c694b65e86927b1219e08300b80f5
Eugene Zaretskiy <eugene.zar@gmail.com>
2014-10-24 11:39:50 -0400

    Updating name

--- config/env/development.js
+++ config/env/development.js
@@ -7,7 +7,7 @@ module.exports = {
     debug: false
   },
   app: {
-    name: 'MEAN - FullStack JS - Development'
+    name: 'Dungeon Lords - Development'
   },
   facebook: {
     clientID: 'DEFAULT_APP_ID',
--- config/env/production.js
+++ config/env/production.js
@@ -27,7 +27,7 @@ module.exports = {
     */
   },
   app: {
-    name: 'MEAN - A Modern Stack - Production'
+    name: 'Dungeon Lords PlayByEmail'
   },
   facebook: {
     clientID: 'APP_ID',
