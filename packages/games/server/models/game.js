de5aba0956acc73b8c7a7f5bd5b0bd07b912e9b8
Eugene Zaretskiy <eugene.zar@gmail.com>
2014-10-24 13:48:30 -0400

    Ranming all instances of article->game

--- packages/games/server/models/game.js
+++ packages/games/server/models/game.js
@@ -8,9 +8,9 @@ var mongoose = require('mongoose'),
 
 
 /**
- * Article Schema
+ * Game Schema
  */
-var ArticleSchema = new Schema({
+var GameSchema = new Schema({
   created: {
     type: Date,
     default: Date.now
@@ -34,21 +34,21 @@ var ArticleSchema = new Schema({
 /**
  * Validations
  */
-ArticleSchema.path('title').validate(function(title) {
+GameSchema.path('title').validate(function(title) {
   return !!title;
 }, 'Title cannot be blank');
 
-ArticleSchema.path('content').validate(function(content) {
+GameSchema.path('content').validate(function(content) {
   return !!content;
 }, 'Content cannot be blank');
 
 /**
  * Statics
  */
-ArticleSchema.statics.load = function(id, cb) {
+GameSchema.statics.load = function(id, cb) {
   this.findOne({
     _id: id
   }).populate('user', 'name username').exec(cb);
 };
 
-mongoose.model('Article', ArticleSchema);
+mongoose.model('Game', GameSchema);
